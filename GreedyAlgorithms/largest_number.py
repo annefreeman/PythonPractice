8fda41506b99f8e3a3688fac0f8bc3d8761282c4
Anne Freeman <annef9816@gmail.com>
2020-10-07 20:26:53 -0400

    remove print statements

--- GreedyAlgorithms/largest_number.py
+++ GreedyAlgorithms/largest_number.py
@@ -5,16 +5,11 @@ import sys
 def get_larger(digit, maxDigit):
     results_greater = ""+str(digit)+str(maxDigit)
     results_maxIsGreater = ""+str(maxDigit)+str(digit)
-
-    print("result_greater", results_greater)
-    print("result maxIsGreater", results_maxIsGreater)
     if(int(results_greater) >= int(results_maxIsGreater)):
     	return bool(True) 
     return bool(False)
     
     
-
-
 def largest_number(a):
     #write your code here
     result = ""
@@ -26,7 +21,6 @@ def largest_number(a):
                 maxDigit = a[digit]
                 maxIndex = digit
         result += str(maxDigit)
-        print("result:", result)
         a[maxIndex] = 0
     return result
 
